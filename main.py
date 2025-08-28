827f2fe72c4a1220c042ffae17e2c3677cbf84ad
mrinmay2004 <mrinmay21012204@gmail.com>
2025-08-28 14:53:23 +0530

    Updated

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ async def contact(
     return {"message": "✅ Your message has been sent successfully!"}
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     port = int(os.environ.get("PORT", 8000))
-#     uvicorn.run("main:app", host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
